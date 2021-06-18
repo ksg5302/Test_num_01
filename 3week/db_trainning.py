a55f4710080da7f3104505a25912a28630d1b6e5
O.G <ksg5302@gmail.com>
2021-06-18 18:02:27 +0900

    20210616 fin, class, database

--- 3week/db_trainning.py
+++ 3week/db_trainning.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from pandas._config import config
+from pandas.io import html
 from bs4 import BeautifulSoup
 import pymysql, calendar, time, json
 import requests
@@ -8,7 +10,7 @@ from threading import Timer
 
 class DBupdater:
     def __init__(self):
-        self.conn = pymysql.connect(host = '', user = '', password = '', 
+        self.conn = pymysql.connect(host = '127.0.0.1', user = 'root', password = 'quddkfl5302!', 
                                     db = "INVESTAR", charset='utf8')
         with self.conn.cursor() as curs:
             sql = """
@@ -73,11 +75,84 @@ class DBupdater:
                     sql = f"REPLACE INTO company_info (code, company, last_update) VALUES('{code}', '{company}', '{today}');"
                     tmnow = datetime.now().strftime('%Y-%m-%d %H:%M')
                     print(f"[{tmnow}] #{idx+1:04d} REPLACE INTO company_info VALUES ({code}, {company}, {today})")
+
                     self.conn.commit()
                     print(' ')
 
-dbu = DBupdater()
-dbu.update_comp_info()
+    def read_naver(self, code, company, pages_to_fetch):
+        try:
+            url = f"http://finance.naver.com/item/sise_day.nhn?code={code}"
+            html = BeautifulSoup(requests.get(url, headers={'User-agent':'Mozilla/5.0'}).text, "lxml")
+            pgrr = html.find("td",class_="pgRR")
+            if pgrr is None:
+                return None
+            s = str(pgrr.a['href']).split('=')
+            lastpage = s[-1]
+            df = pd.DataFrame()
+            pages = min(int(lastpage), pages_to_fetch)
+            for page in range(1,pages +1):
+                pg_url = '{}&page={}'.format(url, page)
+                df = df.append(pd.read_html(requests.get(pg_url,headers={'User-agent':'Mozilla/5.0'}).text)[0])
+                tmnow = datetime.now().strftime('%Y-%m-%d %H:%M')
+                print('[{}] {} ({}) : {:04d}/{:04d} pages are downloading...'.format(tmnow, company, page, pages), end="\r")
+
+            df = df.rename(columns={'날짜':'date','종가':'close','전일비':'diff','시가':'open','고가':'high','저가':'low','거래량':'volume'})
+            df['date'] = df['date'].replace('.','-')
+            df = df.dropna()
+            df[['close','diff','open','high','low','volume']] = df[['close','diff','open','high','low','volume']].astype(int)
+            df = df[['close','diff','open','high','low','volume']]
+
+        except Exception as e:
+            print('Exception occured : ',str(e))
+            return None
+
+    def replace_into_db(self, df, num, code, company):
+        with self.conn.cursor() as curs:
+            for r in df.itertuples():
+                sql = f"REPLACE INTO daily_price VALUES ('{code}',{r.date},{r.date},{r.open},{r.high},{r.row},{r.close},{r.diff},{r.volume});"
+                curs.execute(sql)
+            self.conn.commit()
+            print('[{}] #{:04d} {} ({}) : {} rows > REPLACE INTO daily_price [OK]'.format(datetime.now().strftime('%Y-%m-%d %H:%M'), num+1, company, code, len(df)))
 
 
+    def update_daily_price(self, pages_to_fetch):
+        for idx, code in enumerate(self.codes):
+            df = self.read_naver(code, self.codes[code], pages_to_fetch)
+            if df is None:
+                continue
+            self.replace_into_db(df, idx, code, self.codes[code])
+        
+    def execute_daily(self):
+        self.update_comp_info
+        try:
+            with open('config.json', 'r') as in_file:
+                config = json.load(in_file)
+                pages_to_fetch = config['pages_to_fetch']
+        except FileNotFoundError:
+            with open('config.json', 'w') as out_file:
+               pages_to_fetch = 100
+               config = {'pages_to_fetch': pages_to_fetch}
+               json.dump(config, out_file)
+        
+        self.update_daily_price(pages_to_fetch)
+        tmnow = datetime.now()
+        lastday = calendar.monthrange(tmnow.year, tmnow.month)[1]
+
+        if tmnow.month == 12 and tmnow.day == lastday:
+            tmnext = tmnow.replace(year=tmnow.year+1, month=1, day=1,hour=17, minute=0,second=0)
+        elif tmnow.day == lastday:
+            tmnext = tmnow.replace(month=tmnow.month+1, day=1, hour=17, minute=0, second=0)
+        else:
+            tmnext = tmnow.replace(day = tmnow.day+1, hour=17, minute=0, second=0)
+        
+        tmdiff = tmnext - tmnow
+        secs = tmdiff.seconds
+        t = Timer(secs, self.execute_daily)
+        print("Waiting for next update ({}) ... ".format(tmnext.strftime('%Y-%m-%d %H:%M')))
+        t.start()
+
+
+if __name__ == '__main__':
+    dbu = DBupdater()
+    dbu.execute_daily()
 
